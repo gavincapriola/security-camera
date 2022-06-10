dda985408996e9405542d9aad95f7a5db1ab079a
Gavin Capriola <gavincapriola@icloud.com>
2022-06-10 14:20:51 -0400

    libraries and setup

---
+++ app/main.py
@@ -0,0 +1,11 @@
+import datetime
+import os
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody.xml")
