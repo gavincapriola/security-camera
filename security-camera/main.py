28d9b0fcc1b5b7ee5e1ba7d3337aacf60c685db3
Gavin Capriola <gavincapriola@icloud.com>
2022-06-17 15:44:52 -0400

    drawing rectangles around detected faces and eyes

--- security-camera/main.py
+++ security-camera/main.py
@@ -7,5 +7,28 @@ cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-body_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_fullbody.xml")
+eye_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_eye.xml")
+
+if not os.path.exists('detections'):
+    os.mkdir('detections')  # create directory if it doesn't exist
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        face_roi = frame[y:y + h, x:x + w]  # region of interest
+        eyes = eye_cascade.detectMultiScale(face_roi)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(face_roi, (ex, ey),
+                          (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
